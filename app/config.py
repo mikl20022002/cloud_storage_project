4779bb4d7e9b626aff47551f74dd6cb27c52c0ce
Chernikh Mikhail <mikl20022002@mail.ru>
2024-12-01 14:23:23 +0300

    add server env and config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
+class DbSettings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_HOST: str
@@ -11,7 +11,16 @@ class Settings(BaseSettings):
     def async_db_url(self):
         return f"postgres+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
-db_settings = Settings()
+class ServerSettings(BaseSettings):
+    SERVER_HOST: str
+    SERVER_PORT: int
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+db_settings = DbSettings()
+server_settings = ServerSettings()
+
